9cdf3d41d71abf40dd7e3dc410d701ee84ef0bdb
ojsc <ojsilvera@gmail.com>
2025-02-18 16:16:25 -0500

    Ajustar logica en js del juego adivina el numero

--- AdivinaNumero/Assets/js/app.js
+++ AdivinaNumero/Assets/js/app.js
@@ -1,100 +1,42 @@
-function cargarTexto(elemento, texto) {
-    document.querySelector(elemento).innerHTML = texto;
-}
-
-
-cargarTexto('h1', 'Juego adivina el numero');
-cargarTexto('.section__p--maxRangeUser', 'Elije el numero mayor para el rango a adivinar');
-cargarTexto('.section__p_intentosUser', 'Elije el numero de intentos');
-
-
-/*
-//Con esto selecciono el selector con nombre h1, la seleccion puede ser el selector de una clase, id, etiqueta, etc.
-let titulo = document.querySelector('h1');
-
-//se le asigna el valor a la variable titulo para que aparezca en el documento html
-titulo.innerHTML = "Juego adivina el numero";
-
-
-//Con esto selecciono el selector p, la seleccion puede ser el selector de una clase, id, etiqueta, etc.
-let input_maxRangeUser = document.querySelector('.section__p--maxRangeUser');
-
-//se le asigna el valor a la variable titulo para que aparezca en el documento html
-input_maxRangeUser.innerHTML = "Elije el numero mayor para el rango a adivinar";
-
-//Con esto selecciono el selector p, la seleccion puede ser el selector de una clase, id, etiqueta, etc.
-let input_intentosUser = document.querySelector('.section__p_intentosUser');
-
-//se le asigna el valor a la variable titulo para que aparezca en el documento html
-input_intentosUser.innerHTML = "Elije el numero de intentos";
-
-//establecemnos la funcion para el boton intentoUsuario
-function intentoUsuario(){
-    alert("Click desde el boton");
-};
-*/
-
-/*
-//Solicita al usuario el valor maximo del rango a usar
-let valorRangoMaximo = parseInt(prompt('Elige el valor maximo del numero a adivinar'));
-
-// definimos la variable que almacena el numero a adivinar, el cual se genera obteniendo la parte entera de un numero
-// random que va del 1 hasta el valor maximo determinado por el usuario
-let numeroSecreto = Math.floor(Math.random() * valorRangoMaximo) + 1;
-
-// estbalecemos el contador de intentos
-let contador = 0;
-
-// definiendo o inicializando la variable para el bucle while
+let numeroSecreto = numeroAleatorio();
+let intentos = 0;
+let maxIntentos = 0;
+let maxRange = 0;
 let numeroUsuario = 0;
 
-//maximo numero de intentos
+const cantiddaIntentos = document.getElementById('intentosUser');
+const valorMaxRange = document.getElementById('maxRange');
+const valorNumeroUsuario = document.getElementById('numeroUser');
 
-let intentosMaximos = 3 ;
+function cargarTexto(elemento, texto) {
+    document.querySelector(elemento).innerHTML = texto;
+    return;
+}
 
-//Reglas del juego
-alert(`Tienes ${intentosMaximos} intentos`);
-
-alert(`El nuemro está entre 1 y ${valorRangoMaximo}`);
-
-// ciclo de preguntas e intentos
-while (numeroUsuario != numeroSecreto) {
-
-    numeroUsuario = parseInt(prompt(`Digita un numero entre 1 y ${valorRangoMaximo}: `));
-
-    if (numeroUsuario == numeroSecreto) {
-        // le indica al usuario que acerto el numero secreto
-        alert(` Has acertado El numero secreto es ${ numeroSecreto } `);
-
-        contador++ ;
-
-        alert(`lo has logrado en tu intento numero: ${contador}`);
-
-    } else {
-        //alert("No acertaste el numero secreto")
-        if (numeroUsuario < numeroSecreto) {
-            // le indica al usuario que su numero es menor que el numero secreto para que lo intente de nuevo
-            alert("tu numero es menor que el numero secreto");
-        } else {
-            // le indica al usuario que su numero es mayor que el numero secreto para que lo intente de nuevo
-            alert("Tu numero es mayor que el numero secreto");
-        }
-
-        contador++;
-
-        if (contador < intentosMaximos) {
-
-            alert(`Tienes: ${intentosMaximos - contador } Intentos restantes`);
-
-        } else {
-
-            alert(`Usaste ${intentosMaximos} intentos, perdiste, el numero secreto era ${numeroSecreto}`);
-
-            break;
-        }
-
-
-    };
-
+function intentoUsuario(){
+    console.log('Maximo numero de Intentos: ' + cantiddaIntentos.value);
+    console.log('Maximo rango: ' + valorMaxRange.value);
+    console.log('Numero usuario: ' + valorNumeroUsuario.value);
+    console.log('------------------------------------');
+    return;
 };
-*/
+
+function numeroAleatorio() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function resetGame(){
+    numeroSecreto = numeroAleatorio();
+    intentos = 0;
+    maxIntentos = 0;
+    maxRange = 0;
+    numeroUsuario = 0;
+    intentoUsuario();
+    return;
+}
+
+cargarTexto('h1', 'Juego adivina el numero');
+cargarTexto('.section__p_intentosUser', 'Elije el numero maximo de intentos');
+cargarTexto('.section__p--maxRangeUser', 'Elije el numero mayor para el rango a adivinar');
+cargarTexto('.section__p_numeroUser', 'Digita tu numero');
+
